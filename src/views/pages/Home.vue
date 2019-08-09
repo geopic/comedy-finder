d6388752aa686871a7b670a656521f17bce92a44
George Pickering <29524044+tedjenkins@users.noreply.github.com>
2019-08-09 12:55:19 +0100

    vue2-leaflet added and integrated into app; map not yet operational

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="home">
-    <div id="map">
-      <!-- Map goes here -->
-    </div>
+    <l-map :center="center" :style="style" :zoom="zoom">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker :lat-lng="marker"></l-marker>
+    </l-map>
     <div id="calendar">
       <!-- Calendar goes here -->
     </div>
@@ -11,7 +12,33 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import L from 'leaflet';
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 
-@Component
-export default class Home extends Vue {}
+@Component({
+  components: {
+    'l-map': LMap,
+    'l-tile-layer': LTileLayer,
+    'l-marker': LMarker
+  }
+})
+export default class Home extends Vue {
+  // l-map props
+  center = L.latLng(47.41322, -1.219482);
+  style = 'border: 1px solid black; height: 50%; width: 50%;';
+  zoom = 13;
+
+  // l-tile-layer props
+  url = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+  attribution = 'Test attribution';
+
+  // l-marker props
+  marker = L.latLng(47.41322, -1.219482);
+}
 </script>
+
+<style lang="scss" scoped>
+#home {
+  height: 100%;
+}
+</style>
