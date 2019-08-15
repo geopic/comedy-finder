718e15139569e5f7b36c3eba1d39b650c7e495ef
George Pickering <29524044+tedjenkins@users.noreply.github.com>
2019-08-15 18:22:12 +0100

    Commencing integration of FullCalendar

--- src/views/pages/Calendar.vue
+++ src/views/pages/Calendar.vue
@@ -4,9 +4,19 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { Calendar as Cal } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component
-export default class Calendar extends Vue {}
+export default class Calendar extends Vue {
+  mounted() {
+    const calendar = new Cal(document.getElementById('calendar')!, {
+      plugins: [dayGridPlugin]
+    });
+
+    calendar.render();
+  }
+}
 </script>
 
 <style lang="scss" scoped>
